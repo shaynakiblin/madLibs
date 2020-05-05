24505c41068c150417f90e09c5c20e54f18be60b
shaynakiblin <skiblin03@gmail.com>
2020-05-05 14:07:36 -0400

    created main program

---
+++ src/madLibs.py
@@ -0,0 +1,7 @@
+"""
+This is a program for a madlibs game, the user can choose or have a madlibs prompt chosen for them
+the user can then fill in the prompt to get a funny story
+
+Author: Shayna Kiblin
+
+"""
