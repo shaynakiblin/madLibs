0edcb01434954637bdafb841ff08d20bf5dc85ee
shaynakiblin <skiblin03@gmail.com>
2020-05-05 14:39:05 -0400

    game can now read simple file

--- src/madLibs.py
+++ src/madLibs.py
@@ -5,3 +5,36 @@ the user can then fill in the prompt to get a funny story
 Author: Shayna Kiblin
 
 """
+
+keys = {
+    "[ADJ]" : "adjective",
+    "[VRB]" : "verb",
+    "[NOUN]" : "noun",
+    "[ADV]" : "adverb",
+    "[PNOUN]" : "proper noun",
+    "[PERS]" : "person",
+    "[CITY]" : "city",
+    "[BODYPART]" : "body part",
+    "[NUM]" : "number",
+    "[PLACE]" : "place"
+
+}
+
+file = open(("/Users/shaynakiblin/PycharmProjects/madLibs/src/prompts/" + "testFile.txt"), 'r')
+
+lines = file.readlines()
+
+for line in lines:
+    line = line.strip()
+    if line in keys:
+        print(keys[line])
+    else:
+        print("not in dict")
+
+
+
+
+
+
+
+file.close()
